70b6c3705998785c1ec0bb0306ef1e4105e724e6
Christopher S. Vesterby <vesterby@hotmail.dk>
2025-09-10 11:31:00 +0200

    Er gået i gang med infinityScroll-02.js

---
+++ infinityScroll-02.js
@@ -0,0 +1,11 @@
+const main = document.querySelector("#main-wrapper")
+let last;
+
+async function getPokens(limit, offset) {
+
+    if (last) return
+    let params = `?offset=${offset}&limit=${limit}`
+
+}
+
+// Nået til 4:36 i Steens video pokemon nr. 6 
